4e145c9062b2d401a5ee6f9e6ce2f05ca8528bfb
Lukas Schulze <820580+lsgd@users.noreply.github.com>
2023-12-03 13:55:16 +0100

    refactor imports from TS to JS

--- node/speech_openai.js
+++ node/speech_openai.js
@@ -1,9 +1,9 @@
-import OpenAI, {toFile} from 'openai';
+const {OpenAI, toFile} = require('openai');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
 
 // Gets API key from environment variable process.env.OPENAI_API_KEY
 const openai = new OpenAI();
-let ffmpeg = require('fluent-ffmpeg')
-let fs = require('fs')
 
 async function transcribe(binaryVoiceBuffer, voiceMessageId, message) {
     return new Promise(async (resolve, reject) => {
@@ -20,7 +20,7 @@ async function transcribe(binaryVoiceBuffer, voiceMessageId, message) {
                     response_format: 'text',
                 });
                 console.log(`Transcription successful! OpenAI Whisper API responded with: ${transcription.text}`);
-                if(fs.existsSync(destFile)) {
+                if (fs.existsSync(destFile)) {
                     fs.unlinkSync(destFile);
                 }
                 resolve(JSON.stringify({'results': [{'filename': destFile, 'transcript': transcription.text}]}));
